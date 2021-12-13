8c8bef88f966604a04a1256decaf5678c4f23ac6
letancuong1 <89792454+letancuong1@users.noreply.github.com>
2021-12-13 09:24:43 +0700

    Update 13skill.js

--- src/pages/13SkillPage/13skill.js
+++ src/pages/13SkillPage/13skill.js
@@ -1,9 +1,11 @@
 import {useState} from 'react'
 export default function Skill() {
 	const [path,setpath]=useState()
-
+	const change=(e)=>{
+		console.log(e.target.files[0].name)
+	}
 	return <div class="container">
-		<input onChange={e=>setpath(e.target.value)} type="file"/>
-		<input value={path} type="text"/>
+		<input onChange={e=>setpath(e.target.files[0].name)} type="file"/>
+		<input value={path?window.location.origin+"/"+`${path}`:""} type="text"/>
 	</div>
 }
